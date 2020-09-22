e5500dd591f59dbafd2427a3984da73c819c528d
Tibor Leupold <tibor@lpld.io>
2020-09-21 18:16:29 -1000

    Add CSRF exemption to wagtail serve view
    I am replacing the Wagtail serve view with itself, but wrapped in the Django CSRF exemption.
    This allows to send POST requests to the Wagtail URLs without having to pull the CRSF token into the forms first.

--- contact_form_prototype/urls.py
+++ contact_form_prototype/urls.py
@@ -1,9 +1,11 @@
 from django.conf import settings
 from django.urls import include, path
 from django.contrib import admin
+from django.views.decorators import csrf as djvdcsrf  # type: ignore[import]
 
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
+from wagtail.core import views as wagtail_views
 from wagtail.documents import urls as wagtaildocs_urls
 
 from grapple import urls as grapple_urls
@@ -29,6 +31,17 @@ if settings.DEBUG:
     # Serve static and media files from development server
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Replace the Wagtail serve view with itself, but wrapped with the Django CSRF
+# exemption. This allows to send POST requests to the Wagtail URLs without
+# having to pull the CRSF token into the forms first. This is makes it easier
+# to use the Wagtail Form builder in a headless setup.
+serve_filtered_wagtail_urls = [
+    url for url in wagtail_urls.urlpatterns if url.name == 'wagtail_serve'
+]
+if serve_filtered_wagtail_urls:
+    serve_url = serve_filtered_wagtail_urls[0]
+    serve_url.callback = djvdcsrf.csrf_exempt(serve_url.callback)
 
 urlpatterns = urlpatterns + [
     # For anything not caught by a more specific rule above, hand over to
