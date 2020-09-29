3cb31865780a8a9e3754739ed7eced141e32830e
Tibor Leupold <tibor@lpld.io>
2020-09-28 18:29:45 -0700

    Rename page for grapple issue and add relation
    For some reason, grapple was stipping the "Content" from the "ContentPage" model.
    This lead to a duplicate schema definition. To get around this, I renamed it to
    the fantastic name StuffPage, which works.
    
    Also, I discorvered this while I was trying to test the created relation from the
    content/stuff page to the contact form. So that is implemented with this commit too.
    ;

--- content/models.py
+++ content/models.py
@@ -4,21 +4,33 @@
 Module for a simple secondary page type the will hold a contact form.
 """
 
+from django.db import models as djm
 from grapple import models as gplm
 from wagtail.core import models as wtm
 from wagtail.core import fields as wtf
 from wagtail.admin import edit_handlers as wtah
 
 
-class ContentPage(wtm.Page):
+class SomePage(wtm.Page):
     """Simple page to hold a form."""
 
     intro = wtf.RichTextField()
+    contact_form = djm.ForeignKey(
+        'forms.FormPage',
+        null=True,
+        blank=True,
+        on_delete=djm.SET_NULL,
+    )
 
     content_panels = wtm.Page.content_panels + [
         wtah.FieldPanel('intro'),
+        wtah.FieldPanel('contact_form'),
     ]
 
     graphql_fields = [
         gplm.GraphQLString('intro'),
+        gplm.GraphQLForeignKey(
+            'contact_form',
+            'forms.FormPage',
+        ),
     ]
