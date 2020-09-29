4af521e8d09e9571c567fdb226d53b83445a55b9
Tibor Leupold <tibor@lpld.io>
2020-09-28 19:23:31 -0700

    Add graph ql field for backwards relation

--- forms/models.py
+++ forms/models.py
@@ -14,6 +14,7 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+
 class FormField(wtfm.AbstractFormField):
     """Define fields available in admin to build form."""
 
@@ -57,6 +58,10 @@ class FormPage(wtfm.AbstractEmailForm):
         gplm.GraphQLString('from_address'),
         gplm.GraphQLString('to_address'),
         gplm.GraphQLString('subject'),
+        gplm.GraphQLPage(
+            'used_on_page',
+            is_list=True
+        )
     ]
 
     spam_protection_field = {
